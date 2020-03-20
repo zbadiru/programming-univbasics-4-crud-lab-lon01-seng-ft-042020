285d877360cfd2c4fd8b2a73f926ef90dca5df9b
Zakari Badiru <zbadiru@live.co.uk>
2020-03-20 11:08:14 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array.push(5)
-  p array
+  array = [1, 2, 3, 4,]
+  array << "5"
 end
 
 def add_element_to_start_of_array(array, element)
